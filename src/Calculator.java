d1731d628366c8fdc88d2f00a784ff1164577444
dhdyd <dhdyd@192.168.2.6>
2019-01-23 15:21:23 +0900

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+
+public class Calculator {
+	public static void main(String[]args) {
+		System.out.println("덧셈 : " + (4+3));
+		System.out.println("뺄셈 : " + (4-3));
+		System.out.println("곱셈 : " + (4*3));
+		System.out.println("나눗셈 : " + (4/2));
+	}
+}
